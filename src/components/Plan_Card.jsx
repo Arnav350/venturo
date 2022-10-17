26a9f9afa818706e512ea9cccf358e08109d69a0
Arnav Patel <patelarnavm+git@gmail.com>
2022-10-16 21:13:42 -0400

    Plan_Card component with props, updated plan jsx and css

---
+++ src/components/Plan_Card.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import { FaCheck } from "react-icons/fa";
+
+function Plan_Card({
+  time = "Biweekly",
+  price,
+  ses = "/Session",
+  length = "1 Hour Sessions",
+  num = "Individual",
+  third = true,
+}) {
+  return (
+    <div className="plan-card">
+      <h3 className="plan-time plan-text">{time}</h3>
+      <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
+      <h1 className="plan-price plan-text">{price}</h1>
+      <h4 className="plan-ses plan-text">{ses}</h4>
+      <Link to="/" className="plan-button blue-button">
+        Choose
+      </Link>
+      <ul className="plan-list">
+        <li className="plan-item">
+          <FaCheck className="plan-check plan-text" />
+          <p className="plan-text">{length}</p>
+        </li>
+        <li className="plan-item">
+          <FaCheck className="plan-check plan-text" />
+          <p className="plan-text">{num}</p>
+        </li>
+        {third ? (
+          <li className="plan-item">
+            <FaCheck className="plan-check plan-text" />
+            <p className="plan-text">Try now for Free</p>
+          </li>
+        ) : null}
+      </ul>
+    </div>
+  );
+}
+
+export default Plan_Card;
--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../App.css";
 import "./Plan.css";
+import Plan_Card from "../components/Plan_Card.jsx";
 
 function Plan() {
   return (
@@ -10,113 +10,17 @@ function Plan() {
         <div className="plan container">
           <h1 className="plan-title">Payment Plans</h1>
           <div className="plan-box">
-            <div className="plan-card">
-              <h3 className="plan-time plan-text">Weekly</h3>
-              <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
-              <h1 className="plan-price plan-text">$22</h1>
-              <h4 className="plan-ses plan-text">/Session</h4>
-              <Link to="/" className="plan-button blue-button">
-                Choose
-              </Link>
-              <ul className="plan-list">
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">1 Hour Sessions</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Individual</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Try now for Free</p>
-                </li>
-              </ul>
-            </div>
-            <div className="plan-card">
-              <h3 className="plan-time plan-text">Biweekly</h3>
-              <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
-              <h1 className="plan-price plan-text">$20</h1>
-              <h4 className="plan-ses plan-text">/Session</h4>
-              <Link to="/" className="plan-button blue-button">
-                Choose
-              </Link>
-              <ul className="plan-list">
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">1 Hour Sessions</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Individual</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Try now for Free</p>
-                </li>
-              </ul>
-            </div>
-            <div className="plan-card">
-              <h3 className="plan-time plan-text">Biweekly</h3>
-              <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
-              <h1 className="plan-price plan-text">$15</h1>
-              <h4 className="plan-ses plan-text">/Session</h4>
-              <Link to="/" className="plan-button blue-button">
-                Choose
-              </Link>
-              <ul className="plan-list">
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">1 Hour Sessions</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Group Lesson</p>
-                </li>
-              </ul>
-            </div>
-            <div className="plan-card">
-              <h3 className="plan-time plan-text">Weekly</h3>
-              <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
-              <h1 className="plan-price plan-text">$25</h1>
-              <h4 className="plan-ses plan-text">/Session</h4>
-              <Link to="/" className="plan-button blue-button">
-                Choose
-              </Link>
-              <ul className="plan-list">
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Half Hour Sessions</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Individual</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Try now for Free</p>
-                </li>
-              </ul>
-            </div>
-            <div className="plan-card">
-              <h3 className="plan-time plan-text">Custom</h3>
-              <h4 className="plan-sub plan-text">Monthly Billing Period</h4>
-              <h1 className="plan-price plan-text">$--</h1>
-              <h4 className="plan-ses plan-text">Click for an estimate</h4>
-              <Link to="/" className="plan-button blue-button">
-                Choose
-              </Link>
-              <ul className="plan-list">
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Any Length</p>
-                </li>
-                <li className="plan-item">
-                  <i className="fa-solid fa-check plan-check plan-text"></i>
-                  <p className="plan-text">Individual</p>
-                </li>
-              </ul>
-            </div>
+            <Plan_Card time="Weekly" price="$22" />
+            <Plan_Card price="$20" />
+            <Plan_Card price="$15" num="Group Lesson" third={false} />
+            <Plan_Card time="Weekly" price="$25" length="Half Hour Sessions" />
+            <Plan_Card
+              time="Custom"
+              price="$--"
+              ses="Click for an estimate"
+              length="Any Length"
+              third={false}
+            />
           </div>
         </div>
       </section>
