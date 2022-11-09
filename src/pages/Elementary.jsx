ec492ba35a7ad3000ac1c10acbcad91549b83967
Arnav Patel <patelarnavm+git@gmail.com>
2022-11-08 22:01:10 -0500

    added problems to elementary

--- src/pages/Elementary.jsx
+++ src/pages/Elementary.jsx
@@ -19,7 +19,21 @@ function Elementary() {
     Math.round((threeA / 4 + threeB / 12 + threeC / 6 + Number.EPSILON) * 100) /
     100;
 
-  const fourA = "temp";
+  const fourA = Math.floor((Math.random() * 6 + 5) * 100) / 100;
+  const fourB = Math.floor(Math.random() * 5 * 100) / 100;
+  const fourAns = fourA - fourB;
+
+  const fiveA = Math.floor(Math.random() * 3);
+  const fiveAns = fiveA === 0 ? "8:30" : fiveA === 1 ? "3:00" : "1:15";
+
+  const sixA = Math.floor(Math.random() * 5 + 2);
+  const sixB = Math.floor(Math.random() * 5 + 1);
+  const sixC = Math.floor(Math.random() * 5 + 3);
+  const sixAns = sixA * sixB * sixC;
+
+  const sevenA = Math.floor((Math.random() * 50 + 51) / 10) * 10;
+  const sevenB = Math.floor(Math.random() * 9 + 1);
+  const sevenAns = (sevenA * sevenB) / 10;
 
   return (
     <>
@@ -36,10 +50,38 @@ function Elementary() {
           </div>
           <h3>Question 3</h3>
           <div>
-            Evaluate and convert to decimal. (round up to 2 decimal places if
+            Evaluate and convert to decimal. (round to 2 decimal places if
             needed) <sup>{threeA}</sup>&frasl;<sub>4</sub> + <sup>{threeB}</sup>
             &frasl;<sub>12</sub> + <sup>{threeC}</sup>&frasl;<sub>6</sub>{" "}
             {threeAns}
+          </div>
+          <h3>Question 4</h3>
+          <div>
+            Andrew wants to buy a bat for {fourA} and pays {fourB}, how much
+            change should he get {fourAns}
+          </div>
+          <h3>Question 5</h3>
+          <div>
+            What time does the clock say?{" "}
+            {fiveA === 0 ? (
+              <img src="" alt="8:30" />
+            ) : fiveA === 1 ? (
+              <img src="" alt="3:00" />
+            ) : (
+              <img src="" alt="1:15" />
+            )}{" "}
+            {fiveAns}
+          </div>
+          <h3>Question 6</h3>
+          <div>
+            What is the area of a rectangular prism with side lengths {sixA}cm,{" "}
+            {sixB}cm, and {sixC}cm? {sixAns}cm^3
+          </div>
+          <h3>Question 7</h3>
+          <div>
+            Isabella is taking a {sevenA} mile long road trip. She is currently{" "}
+            <sup>{sevenB}</sup>&frasl;<sub>10</sub> of the way through her road
+            trip. How many miles has she traveled so far? {sevenAns}
           </div>
         </section>
       </main>
