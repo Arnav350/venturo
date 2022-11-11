1eba2def3996ea2b4b065acc61f90a56c11e3619
Arnav Patel <patelarnavm+git@gmail.com>
2022-11-10 22:27:07 -0500

    added names to account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -28,14 +28,24 @@ function Account() {
               <h1 className="account-title">Personal Information</h1>
               <img src={Profile1} alt="Profile" className="account-profile" />
               <div className="account-row">
-                <h3 className="account-header">Email Address</h3>
+                <h3 className="account-item">First Name</h3>
+                <h3 className="account-item">Last Name</h3>
                 <button className="account-edit blue-button">Edit</button>
               </div>
+              <div className="account-names">
+                <label className="account-name">
+                  <input type="text" className="account-first" readOnly />
+                </label>
+                <label className="account-name">
+                  <input type="text" className="account-last" readOnly />
+                </label>
+              </div>
+              <h3 className="account-item">Email Address</h3>
               <label className="account-mail">
                 <input type="text" className="account-email" readOnly />
               </label>
               <div className="account-row">
-                <h3 className="account-header">Password</h3>
+                <h3 className="account-item">Password</h3>
                 <button className="account-edit blue-button">Edit</button>
               </div>
               <label className="account-pass">
